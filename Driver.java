0c043b4fa9e907228410959bb228d33e4fec8e4f
John Bouyer <109704002+Turfader@users.noreply.github.com>
2023-04-18 19:32:11 -0700

    Added some more guidance

--- Driver.java
+++ Driver.java
@@ -2,7 +2,32 @@
 
 public class Driver {
   public static void main(String[] args) {
-      System.out.println("Hello, world!");
-    // your code here
+      // your code here
+      // you should create an array that can hold 3 dog objects called dogs
+      // the 0th and 2nd dogs can be generic, but the 1st dog should have a name parameter. Name this dog "Spot"
+    
+      System.out.println(dogs[0].getHappiness());
+      System.out.println(dogs[1].getName());
+      System.out.println(dogs[2].getLegs());
+      System.out.println();
+
+      dogs[0].isHappy = false;
+      dogs[1].name = "Fido";
+      dogs[2].legs = 3;
+
+      System.out.println(dogs[0].getHappiness());
+      System.out.println(dogs[1].getName());
+      System.out.println(dogs[2].getLegs());
+      System.out.println();
+        
+      /* expected print:
+      true
+      Spot
+      4
+      
+      false
+      Fido
+      3
+      */
    }
 }
