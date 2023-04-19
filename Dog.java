7543564dc69619ef25541c4de5603bc7925c6ae0
John Bouyer <109704002+Turfader@users.noreply.github.com>
2023-04-18 19:52:11 -0700

    Added additional guidance comments

--- Dog.java
+++ Dog.java
@@ -1,5 +1,19 @@
 public class Dog {
+  
+  //define attribute types here. Do not instantiate!
+  
   public Dog() {
-    // attributes here
+    
+    //assign attrubutes here
+    
   }
+  
+  public Dog(/*add parameters here*/) {
+    
+    //assign attributes here
+    
+  }
+  
+  //add get functions here
+  
 }
